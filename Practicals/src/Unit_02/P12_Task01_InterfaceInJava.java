4ddb1c1a53bf42ddd001f06bc8a1be3f50e00916
Kanchan-Bartwal <kanchanbartwal10@gmail.com>
2022-04-16 10:31:44 +0530

    new class

---
+++ Practicals/src/Unit_02/P12_Task01_InterfaceInJava.java
@@ -0,0 +1,72 @@
+package Unit_02;
+
+interface Animal
+{
+	public void animalSound();
+	public void run();
+}
+
+interface Human
+{
+	public void humanSound();
+	public void run1();
+}
+public class P12_Task01_InterfaceInJava implements Animal{
+
+	public static void main(String[] args) 
+	{
+		C1 obj = new C1();
+		obj.funcA();
+		obj.funcB();
+	}
+	public void animalSound()
+	{
+		
+	}
+	public void run()
+	{
+		
+	}
+}
+class Species implements Animal, Human
+{
+	public void animalSound()
+	{
+		System.out.println("INSIDE animalSound method");
+	}
+	
+	public void run()
+	{
+		System.out.println("INSIDE run method");
+	}
+	public void humanSound()
+	{
+		System.out.println("INSIDE humanSound");
+	}
+	public void run1()
+	{
+		System.out.println("INSIDE run1 method");
+	}
+}
+
+interface A1
+{
+	void funcA();
+}
+
+interface B1 extends A1
+{
+	void funcB();
+}
+class C1 implements B1
+{
+	public void funcA()
+	{
+		System.out.println("This is funcA from A1");
+	}
+	public void funcB()
+	{
+		System.out.println("This is funcB from A2");
+	}
+	
+}
